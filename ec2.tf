4e89e937b00b40e62c4f87c8f16ce82d88cf42ef
Pranav Kumar <pranav.das@hotmail.com>
2026-01-12 09:00:53 +0530

    Change instance type from t3.micro to t3.nano

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "example" {
   ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
-  instance_type = "t3.micro"
+  instance_type = "t3.nano"
 
   tags = {
     Name = "HelloWorld"
